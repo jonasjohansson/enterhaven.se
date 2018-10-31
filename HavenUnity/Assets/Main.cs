e941c750a2427af766ccc4e4f5d0edf078557829
Jonas Johansson <jns.johansson@gmail.com>
2018-10-31 17:24:02 +0100

    Basic GUI

---
+++ HavenUnity/Assets/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+
+    public Canvas GUI;
+    public Button enterButton;
+
+    void Start()
+    {
+        enterButton.onClick.AddListener(() =>
+        {
+            GUI.GetComponent<CanvasGroup>().alpha = 0;
+            //GameObject.Find("Button").GetComponent<Image>().CrossFadeAlpha(0.1f, 2.0f, false);
+        });
+    }
+
+    void Update()
+    {
+    }
+}
