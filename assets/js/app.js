b231074a6a2be9fb96758e4750bb68f1e92d1026
Jonas Johansson <jns.johansson@gmail.com>
2018-12-14 20:07:43 +0100

    Simple audio play

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 function enterHaven() {
-	var $intro = document.querySelector('#intro');
-	$intro.classList.add('fade-out');
-	window.startSoundscape();
+	// var $intro = document.querySelector('#intro');
+	// $intro.classList.add('fade-out');
+	// window.startSoundscape();
+	var $audio = document.querySelector('audio');
+	$audio.play();
 }
