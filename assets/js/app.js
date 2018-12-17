f67e67bf14428417e0145b807ebb9c0cb40dbe5f
Jonas Johansson <jns.johansson@gmail.com>
2018-12-17 21:38:01 +0100

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,19 @@
-function enterHaven(timeOfDay) {
-    console.log(timeOfDay);
-    var $intro = document.querySelector('#intro');
-    $intro.classList.add('fadeOut');
-    // window.startSoundscape();
-    var $audio = document.querySelector('audio');
-    $audio.play();
+var $intro, $audio;
+var $playBtn;
+
+window.onload = () => {
+	document.body.classList.remove('loading');
+	$playBtn = document.querySelector('button');
+	$playBtn.addEventListener('click', playAudio);
+	let now = new Date();
+	let h = now.getHours();
+	if (h >= 16 || h <= 6) document.documentElement.className = 'night';
+};
+
+function playAudio() {
+	$intro = document.querySelector('#intro');
+	$audio = document.querySelector('audio');
+	$playBtn.classList.add('fadeOut');
+	$playBtn.setAttribute('disabled', true);
+	$audio.play();
 }
