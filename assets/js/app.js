5ea8cfa7563c828e34f1aa1857b94c9aab1cd09f
Jonas Johansson <jns.johansson@gmail.com>
2018-12-17 15:01:25 +0100

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
-function enterHaven() {
-	// var $intro = document.querySelector('#intro');
-	// $intro.classList.add('fade-out');
-	// window.startSoundscape();
-	var $audio = document.querySelector('audio');
-	$audio.play();
+function enterHaven(timeOfDay) {
+    console.log(timeOfDay);
+    var $intro = document.querySelector('#intro');
+    $intro.classList.add('fadeOut');
+    // window.startSoundscape();
+    var $audio = document.querySelector('audio');
+    $audio.play();
 }
